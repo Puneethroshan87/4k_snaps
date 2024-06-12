8a5048e2668dd30d43ba42d3a6341e942c3a3762
puneethroshan87 <puneethroshansangireddy87@gmail.com>
2024-06-12 15:01:56 +0530

    reactproject

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import SearchBar from './components/input';
+import MainPage from './components/main';
+
+function App() {
+  return (
+<>
+
+    {/* <SearchBar/> */}
+    
+<div className='center'>
+    
+
+<MainPage></MainPage>
+</div>
+
+</>
+  );
+}
+
+export default App;
